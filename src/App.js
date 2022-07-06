411b8682ab749c30fb8d60e4bb9e85a0b90a07da
Taraka Pavan <tarakapavanb@gmail.com>
2022-07-06 20:42:49 +1000

    This is my First commit for tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState,useRef} from 'react';
+import TodoList from './TodoList';
+import uuidv4 from './v4';
 function App() {
+  const [todos,setTodos]=useState([])
+  const todoNameRef = useRef()
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if(name === '') return
+    setTodos(prevTodos =>{
+      return [...prevTodos,{id:uuidv4(), name:name,complete:false}]
+    })
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <TodoList todos={todos} />
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo}>Add Todo</button> 
+    <button>Clear Complete</button>
+    <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
